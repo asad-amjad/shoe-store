f89d6db588540347f0eafd836215062fb742ff6e
Asad <asad.amjad.pk@gmail.com>
2023-04-14 08:47:48 +0500

    config

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
+import Menu from "./Menu";
+
+function Header() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper>
+        <Link href={"/"}>
+          <img src="/logo.svg" className="w-[40px] md:[60px]" />
+        </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+      </Wrapper>
+    </header>
+  );
+}
+
+export default Header;
